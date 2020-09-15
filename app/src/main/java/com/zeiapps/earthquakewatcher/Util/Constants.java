bee45ce595c0b2a8577049bbfc72942f36ae8da3
jupiter <fzeisaada@gmail.com>
2020-09-15 20:58:40 -0300

    App is running

---
+++ app/src/main/java/com/zeiapps/earthquakewatcher/Util/Constants.java
@@ -0,0 +1,15 @@
+package com.zeiapps.earthquakewatcher.Util;
+
+import java.util.Random;
+
+public class Constants {
+
+    public static final String URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+    public static final int LIMIT = 30; //it will limit the earthquakes exhibited
+
+    public static int randomInt (int max, int min){
+
+        return new Random().nextInt(max - min) + min;
+    }
+
+}
